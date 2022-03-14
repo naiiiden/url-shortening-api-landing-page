4abf2a1b22675adf40ae1ca2815fdf58f66312a1
naiden <na4ko359@gmail.com>
2022-03-14 12:49:31 +0200

    mobile nav/transition

--- script/script.js
+++ script/script.js
@@ -1,3 +1,12 @@
+document.querySelector("#button").addEventListener("click", () => {
+    document.querySelector("#nav-ul").classList.toggle("show");
+    if (document.getElementById("menu-icon").getAttribute("src") === "images/menu_black_36dp.svg") {
+        document.getElementById("menu-icon").setAttribute("src", "images/close_black_36dp.svg");
+    } else {
+        document.getElementById("menu-icon").setAttribute("src", "images/menu_black_36dp.svg");
+    };
+});
+
 // document.getElementById("myinput").addEventListener("click", () => {
 //     let link = document.getElementById("linkinput").value;
 //     let data = {
